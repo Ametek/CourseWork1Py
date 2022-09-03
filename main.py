19a3b29cb1095c836a5582ade035b78a42d4225f
Dmitriy Kurzykin <ametek84@mail.ru>
2022-09-04 01:29:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+
+class VKDownloader:
+    def __init__(self, token):
+        self.token = token
+
+
+
+if __name__ == '__main__':
+    vk_user_id = input('Введите id пользователя ВК: ')
+    ya_token = input('Введите токен ЯндексДиск: ')
