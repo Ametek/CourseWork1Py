29d3210642b4eeacf0c773441984864d76af7793
Dmitriy Kurzykin <ametek84@mail.ru>
2022-09-12 20:35:43 +0300

    modified main.py (add YaUploader). Not work

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ class VKDownloader:
         # print(count_all)  # Удалить
         photos = []  # Полученные фото
         max_photo = {}  # Фото с максимальным разрешением
+
         print('Обработка данных:')
-        for photo in tqdm(load['response']['items']): # Ходим по фоткам
+        for photo in tqdm(load['response']['items'], ncols=93, unit_scale=True): # Ходим по фоткам
             max_size = 0
             # print(photo)  # удалить
             for size in photo['sizes']:  # Ищем максимальный размер
@@ -46,14 +47,17 @@ class VKDownloader:
             # print(max_size)  # удалить
             info['size'] = size['type']
             photos.append(info)  # Добавление в список в требуемом виде
+
         with open('vk_photo_backup.json', 'w') as file:
             json.dump(photos, file, indent=4, sort_keys=False)  # сохраняем .json
+
         time.sleep(0.1)
         print('Скачивание фото:')
-        for name, url in tqdm(max_photo.items()):
+        for name, url in tqdm(max_photo.items(), ncols=100, unit_scale=True):
             with open(f'vk_photo_backup/{name}.jpg', 'wb') as file:
                 image = requests.get(url)
                 file.write(image.content)  # Сохраняем фото в папку
+        return
 
 
         #
@@ -62,6 +66,35 @@ class VKDownloader:
         # print(max_photo)
         #
 
+class YaUploader:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {'Content-Type': 'application/json',
+                'Authorization': 'OAuth {}'.format(self.token)}
+
+    def _get_upload_link(self, path_to_ya: str):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': path_to_ya, 'overwrite': 'True'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def create_folder(self, patch_to_ya: str):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        headers = self.get_headers()
+        params = {'path': patch_to_ya, 'overwrite': 'False'}
+        requests.put(url=url, headers=headers, params=params)
+
+    def upload_file(self, path_to_file):
+        path_to_ya = 'vk_photo_backup'
+        self.create_folder(patch_to_ya=path_to_ya)
+        href = self._get_upload_link(path_to_ya=path_to_ya).get('href', '')
+        response = requests.put(href, data=open(path_to_file, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Резервное копирование успешно завершено')
 
 
 
@@ -70,9 +103,19 @@ class VKDownloader:
 if __name__ == '__main__':
     # vk_user_id = input('Введите id пользователя ВК: ')
     vk_user_id = '57629630'
-    # ya_token = input('Введите токен ЯндексДиск: ')
+    ya_token = input('Введите токен ЯндексДиск: ')
+    # ya_token = 'test'
     with open('vktoken.txt', 'r') as file:
         vktoken = file.readline().strip()
     downloader = VKDownloader(vktoken)
-    print(downloader.get_all_photo())
+    downloader.get_all_photo()
+    uploader = YaUploader(ya_token)
+    folder_contents = os.listdir('vk_photo_backup')
+    for file in folder_contents:
+        file_name = file
+        file_path = './vk_photo_backup/' + file
+        print(file_path)
+        uploader.upload_file(path_to_file=file_path)
+    print('Все операции успешно завершены')
+    print(f'На ЯДиск загружено {len(folder_contents)} фото')
 
